19161632911f357ef331789911d1d9f2592880b5
Andres Bermudez Muñoz <93688095+andresbermudezm@users.noreply.github.com>
2022-03-22 19:43:30 -0500

    Update RSA.py

--- RSA.py
+++ RSA.py
@@ -3,20 +3,24 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 import binascii
- 
+
 keyPair = RSA.generate(3072)
 
 pubKey = keyPair.publickey()
-print(f"Public key:  (n={hex(pubKey.n)}, e={hex(pubKey.e)})")
+print(f"Public key: (n={hex(pubKey.n)}, e={hex(pubKey.e)})")
 pubKeyPEM = pubKey.exportKey()
 print(pubKeyPEM.decode('ascii'))
- 
+
 print(f"Private key: (n={hex(pubKey.n)}, d={hex(keyPair.d)})")
 privKeyPEM = keyPair.exportKey()
 print(privKeyPEM.decode('ascii'))
- 
-#encryption
-msg = 'A message for encryption'
+
+
+msg = bytes(str(input("Enter plain text: ")), 'utf-8')
 encryptor = PKCS1_OAEP.new(pubKey)
 encrypted = encryptor.encrypt(msg)
 print("Encrypted:", binascii.hexlify(encrypted))
+
+decryptor = PKCS1_OAEP.new(keyPair)
+decrypted = decryptor.decrypt(encrypted)
+print('Decrypted:', decrypted.decode('utf-8'))
